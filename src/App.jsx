6b5209cfcb9d164b8271d95ea8df53ea972e0e82
syamalatha gudepu <syama8031997@gmail.com>
2026-06-11 05:54:32 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,73 @@
-import Header from "./components/Header";
+// // import StudentCard from './components/StudentCard';
+
+// // function App() {
+// //   const studentName = "Arun";
+// //   const course = "React JS";
+// //   const age = 21;
+// //   const city = "Coimbatore";
+
+// //   return <StudentCard
+// //     studentName={studentName}
+// //     course={course}
+// //     age={age}
+// //     city={ city }
+// //   />
+
+// // }
+// // export default App;
+
+
+// import { useState } from "react";
+
+// const [reactions, setReactions]=useState({
+//   likes: 0,
+//   dislikes: 0,
+//   history: []
+// });
+
+//   const handleLike = () => {
+//     setLikes(likes + 1);
+//     setHistory([
+//       ...history,
+//       "L"
+//     ]);
+
+//   }
+//   const handleDisLike = () => {
+//     setDisLikes(dislikes + 1);
+//      setHistory([
+//       ...history,
+//       "D"
+//     ]);
+//   }
+
+//   console.log(history);
+
+//   return (
+//     <>
+      
+//     <button onClick={handleLike}>Like {likes}</button> &nbsp;
+//     <button onClick={handleDisLike}>DisLike{dislikes}</button>
+//     <p> History of Clicks: {history} </p>
+//     </>
+//   )
+// }
+
+// export default App;
+
+import Header from "./components/Header"
 
 function App() {
-
-  return ( 
+  const loggedInUser = {
+                         name: "krish",
+                         email: "krishna@guvi.in"
+                        }
+                      
+return (
   <>
-    < Header />
-    < Header />
-
-    </>
-    )
-   
-  
-}
+    <Header
+        loggedInUser={loggedInUser}/>
+   </>
+        )        }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
-function Header() {
-  return (
-    <>
-    <h1>Hello Thanvi & Jishnu!</h1>
-    <p>Good Day!</p>
+// function StudentCard({studentName,course,age,city}) {
+//     console.log(studentName, course, age, city);
 
+//     return <h1>Hello {studentName}!</h1>
+
+// }
+
+// export default StudentCard;
+
+function Header(props)  {
+
+    return(
+    <>
+    <p>Welcome {props.loggedInUser.name}
+          <button>logout</button></p>
     </>
-  )
+)
 }
 
 export default Header;
