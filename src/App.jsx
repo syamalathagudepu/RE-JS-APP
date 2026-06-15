7f462cc0c89d797f0f64aab9aa7a3b5ac06b7f99
syamalatha gudepu <syama8031997@gmail.com>
2026-06-15 18:46:59 +0530

    ComponentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,22 @@
-// import { useState } from "react";
+import {Component} from "react";
 
-// const [reactions, setReactions]=useState({
-//   likes: 0,
-//   dislikes: 0,
-//   history: []
-// });
+export class App extends Component {
+ 
+  componentDidMount () {
+    console.assertlog("component did mount");
+  }
 
-//   const handleLike = () => {
-//     setLikes(likes + 1);
-//     setHistory([
-//       ...history,
-//       "L"
-//     ]);
+ 
 
-//   }
-//   const handleDisLike = () => {
-//     setDisLikes(dislikes + 1);
-//      setHistory([
-//       ...history,
-//       "D"
-//     ]);
-//   }
-
-//   console.log(history);
-
-//   return (
-//     <>
-      
-//     <button onClick={handleLike}>Like {likes}</button> &nbsp;
-//     <button onClick={handleDisLike}>DisLike{dislikes}</button>
-//     <p> History of Clicks: {history} </p>
-//     </>
-//   )
-// }
-
-// export default App;
-
-import Child from "./components/Child";
-
-const App = () => {
-
-function callback(data) {
-  // console.log("Callback applied!");
-  // console.log(data);
-}
-
-  return (
-    <>
-     <Child callback={callback}/>
-     </>
-  )
+  }
+ 
+  render() {
+    return (
+      <>
+      <h1>Hello World!</h1>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+const NavBar = (props) => {
+
+  return(
+
+    <>    
+    <p>Welcome {props.name} <button>logout</button> </p>
+    
+    </>
+    )
+  }
+
+
+export default NavBar;
