0d50baef0b3b104e58b6f12bb8ba9609c1bf3671
syamalatha gudepu <syama8031997@gmail.com>
2026-06-18 06:13:56 +0530

    list rendering values

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-import {useState} from 'react';
+// import React from 'react'
 
 const App = () => {
 
-const [reactions, setReactions] = useState({
-  likes: 0,
-  dislikes: 0,
-  history: []
-});
-
-const handleLike = () => {
-    setReactions({
-      ...reactions,
-      likes: reactions.likes + 1,
-      history: [
-        ...reactions.history,
-        'L'
-      ]
-    });
- }
-const handleDislike = () => {
-    setReactions ({
-      ...reactions,
-      dislikes: reactions.dislikes+1,
-      history: [
-        ...reactions.history,
-        'D'
-      ]
-      
-    });
-}
+const menulist = [
+  'Home',
+  'Register',
+  'Login',
+];
 
   return (
-   
     <>
-    <button onClick={handleLike}>Like {reactions.likes}</button> &nbsp;
-    <button onClick={handleDislike}>Dislike {reactions.dislikes}</button>
-    <p>History of Clicks: {reactions.history.join(', ') }</p>
+    
+    <h1>Guvi - React</h1>
+    <ul>
+      {
+      menulist.map((value, index)=>{
+        return <li key= {index}><a href="#">{value}</a></li> 
+      })
+    }
+    </ul>
     
     </>
+    
   )
 }
 
