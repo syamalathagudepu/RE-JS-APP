282ae3ebe0b47d7dc1ca2e9a2d4d0775317538ac
syamalatha gudepu <syama8031997@gmail.com>
2026-06-22 12:36:22 +0530

    Like, DisLike and Reset buttons added using UseReducer Hook method

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import { useEffect,useState } from "react";
+import {useReducer} from 'react'
 
 const App = () => {
 
-  const [likes,setLikes] = useState(0);
-
-
-useEffect(() => {
-console.log("component is mounted/updated");
-});
-
-const handleLike = () => {
-    setLikes(likes+1);
+const reducer = (state, action) => {
+   
+  if (action.type === "LIKE") {
+      return state + 1;
+} else if (action.type == "DISLIKE") {
+  return state - 1;
+} else if (action.type == "RESET") {
+  return 0;
 }
 
-  return (
-    
-    <>
-    <h1>Likes : {likes}</h1>
-    <button onClick={handleLike}>Like</button>
-    </>
+return state;
+}
 
+  let [likes, setLikes] = useReducer(reducer , 0);
+  
+  const handleLike = () => {
+        setLikes({type : "LIKE"});
+  }
+  const handleDislike = () => {
+        setLikes({type : "DISLIKE"});
+  }
+
+  const handleReset = () => {
+    setLikes ({type : "RESET"});
+  }
+
+  return (
+    <div>
+      <h1>Likes:{likes}</h1>
+      <button onClick={handleLike}>Like</button> &nbsp;
+      <button onClick={handleDislike}>Dislike</button> &nbsp;
+      <button onClick={handleReset}>Reset</button>
+       </div>
   )
 }
 
