36f5aa29e135cd38337aa558e0bfa8d314c8db71
syamalatha gudepu <syama8031997@gmail.com>
2026-06-30 17:09:54 +0530

    uncontrolled inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import {useReducer} from 'react'
+import {useState} from 'react';
 
 const App = () => {
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
 
-const reducer = (state, action) => {
-   
-  if (action.type === "LIKE") {
-      return state + 1;
-} else if (action.type == "DISLIKE") {
-  return state - 1;
-} else if (action.type == "RESET") {
-  return 0;
+const handleLogin = (e) => {
+  e.preventDefault();
 }
 
-return state;
-}
-
-  let [likes, setLikes] = useReducer(reducer , 0);
-  
-  const handleLike = () => {
-        setLikes({type : "LIKE"});
-  }
-  const handleDislike = () => {
-        setLikes({type : "DISLIKE"});
-  }
-
-  const handleReset = () => {
-    setLikes ({type : "RESET"});
-  }
+console.log("logging in with...");
+console.log(email,password);
 
   return (
-    <div>
-      <h1>Likes:{likes}</h1>
-      <button onClick={handleLike}>Like</button> &nbsp;
-      <button onClick={handleDislike}>Dislike</button> &nbsp;
-      <button onClick={handleReset}>Reset</button>
-       </div>
+    <>
+    <h1>Login</h1>
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        placeholder="Email..."
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+
+      &nbsp;
+      <input
+        type="password"
+        placeholder="Password..."
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      &nbsp;
+      <button
+          type="submit"
+        >Login</button>
+
+    </form>
+
+
+    </>
+    
   )
 }
+
 
 export default App;
