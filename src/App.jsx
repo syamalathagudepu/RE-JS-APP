100be961cfd2becd0b2c7100172f8d9cd553c2ec
syamalatha gudepu <syama8031997@gmail.com>
2026-07-01 06:27:32 +0530

    cleanup for vibe

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import { useState } from "react";
+import React from 'react'
 
 const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    
-    console.log('logging in with...');
-    // console.log(e.target.email.value, e.target.password.value);
-    console.log(email, password);
-  }
-
   return (
-    <>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email..."
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />  
-        &nbsp;
-        <input
-          type="password"
-          placeholder="Password..."
-          name="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-        &nbsp;
-        <button
-          type="submit"
-        >Login</button>
-      </form>
-    </>
+    <div>App</div>
   )
 }
 
